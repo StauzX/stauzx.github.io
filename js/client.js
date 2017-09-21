115bbb168601023b230ba848cbdb13125a7575c3
Yun Tian Xu <yuntianx@usc.edu>
2017-09-20 22:56:15 -0700

    New look

--- js/client.js
+++ js/client.js
@@ -1,6 +1,36 @@
-$('document').ready(function(event){
-    $('.explore-more').click(function(event){
-        $.fn.fullpage.moveSectionDown();
+$(document).ready(function(){
+    currentModal = $('.main');
+    currentModalName = 'main';
+    $('li.menu-item').click(function(){
+        var item = $(this);
+        var target = item.data('target');
+        openModal(target);
     });
-    $('#fullpage').fullpage();
 });
+
+var currentModal;
+var currentModalName;
+
+function openModal(target){
+    if(currentModalName === target){
+        return;
+    }
+    shrink(currentModal, function(){
+        grow(currentModal);
+    });
+    currentModal = $('.modal#modal-' + target);
+    currentModalName = target;
+}
+
+function closeModal(target){
+    shrink(target);
+    grow($('.main'));
+}
+
+function grow(target, callback){
+    target.fadeIn(250, callback);
+}
+
+function shrink(target, callback){
+    target.fadeOut(250, callback);
+}
